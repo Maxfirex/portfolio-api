ed78df49234fad983558c088086ee9e8f7d8bd46
Maxfirex <35975537+Maxfirex@users.noreply.github.com>
2021-07-15 23:07:50 +0200

    Add project files.

---
+++ portfolio-api/Data/UsersContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using portfolio_api.Models;
+
+namespace portfolio_api.Data
+{
+    public class UsersContext : DbContext
+    {
+        public UsersContext(DbContextOptions<UsersContext> options)
+            : base(options) {}
+        public DbSet<User> Users { get; set; }
+    }
+}
